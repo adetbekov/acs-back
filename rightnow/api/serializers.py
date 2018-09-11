1c933b7276d1c3ad4361ff825876a9171117b7ef
adetbekov <dosya@inbox.ru>
2018-09-12 00:51:01 +0600

    Update serializer

--- rightnow/api/serializers.py
+++ rightnow/api/serializers.py
@@ -3,6 +3,14 @@ from rest_framework import serializers
 
 
 class RightnowSerializer(serializers.ModelSerializer):
+	locale_code = serializers.SerializerMethodField()
+
 	class Meta:
 		model = Rightnow
-		fields = ['content', 'mood', 'created']
+		fields = ['content', 'mood', 'locale_code', 'created']
+
+	def get_locale_code(self, obj):
+		if obj.language:
+			return obj.language.code
+		else:
+			return 'ru'
